0cfa73a5b4d87df34293b397ebb81a4bee7335cf
MarkoVukosavic08 <150249704+MarkoVukosavic08@users.noreply.github.com>
2025-09-23 09:17:51 +0200

    Add files via upload

---
+++ skripta2.js
@@ -0,0 +1,20 @@
+// Varijable
+let ime = "Ivo";
+let prezime = "Ivić";
+let a = 2;
+let b = 5;
+let c = a + b;
+
+// Konstante 
+const pi = 3.14;
+const oib = 12345678991;
+
+// Ispis varijabli
+console.log();
+console.log(prezime);
+console.log(c);
+console.log(pi)
+console.log(oib);
+
+// Ispis na web stranici
+document.getElementById("ispis").innerHTML = ime;
