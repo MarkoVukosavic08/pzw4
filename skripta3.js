0ee5f66c9bcb2efdcb8699a29190ce6d712a4938
MarkoVukosavic08 <150249704+MarkoVukosavic08@users.noreply.github.com>
2025-09-30 09:36:18 +0200

    Add files via upload

---
+++ skripta3.js
@@ -0,0 +1,23 @@
+let a = 83;
+let b = 3.14;
+
+let boja = "Žuta";
+let prezime = "Horvat";
+
+let x = true;
+let y = false;
+
+const auti = ["BMW","Audi","Mercedes"];
+
+const osoba = {ime:"Ivo" , prezime:"Ivić", razred:"4.a"};
+
+const date = new Date("2025-09-30");
+
+console.log(a);
+
+console.log(auti[2]);
+
+console.log (osoba.ime);
+console.log (osoba.prezime);
+
+console.log (date);
