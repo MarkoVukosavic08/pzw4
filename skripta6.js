e89f3ff465fa78fcf906f76699dc246c92a73010
MarkoVukosavic08 <150249704+MarkoVukosavic08@users.noreply.github.com>
2025-11-04 09:35:19 +0100

    Add files via upload

---
+++ skripta6.js
@@ -0,0 +1,28 @@
+// Funkcije u JS
+
+// Definiranje funkcije
+function mojaFunkcija() {
+
+  console.log("Pozdrav iz funkcije!");
+}
+
+// Pozivanje funkcije 
+mojaFunkcija()
+
+// Funkcije s argumentima
+function zbroji(a,b) {
+  return a + b;
+}
+
+console.log(zbroji(2,3));
+console.log(zbroji(345,678));
+console.log(zbroji(23,31));
+
+function pozdrav(ime) {
+  console.log(" Dobar dan " + ime + "!");
+}
+
+pozdrav("Ivo");
+pozdrav("Ana");
+pozdrav("Filip");
+
